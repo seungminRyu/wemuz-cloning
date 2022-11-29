a6639a4b112252e61da064c8c57718c3dc933238
heosooyeon <80025144+heosooyeon@users.noreply.github.com>
2022-11-29 15:25:28 +0900

    Edit img soure, img function and palette

--- src/components/chocomusic/ChocomusicIntroTextSection1.tsx
+++ src/components/chocomusic/ChocomusicIntroTextSection1.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import media from "../../lib/styles/media";
 import palette from "../../lib/styles/palette";
-import IntroImg from "../../static/imgs/chocomusic/IntroText1.png";
-import IntroImg2 from "../../static/imgs/chocomusic/IntroText2.png";
-import IntroMob from "../../static/imgs/chocomusic/IntroMobile1.png";
-import IntroMob2 from "../../static/imgs/chocomusic/IntroMobile2.png";
+import introimg1 from "../../static/imgs/chocomusic/IntroText1.png";
+import introimg2 from "../../static/imgs/chocomusic/IntroText2.png";
+import intromobimg1 from "../../static/imgs/chocomusic/IntroMobile1.png";
+import intromobimg2 from "../../static/imgs/chocomusic/IntroMobile2.png";
 
 
 export type ChocomusicIntroTextSection1Prop = {
@@ -22,12 +22,12 @@ function ChocomusicIntroTextSection1(props: ChocomusicIntroTextSection1Prop) {
                     <Text active={active}>아티스트와 관객이 함께한 값진 순간을<br></br>감성을 불어넣은 특별한 공연으로 완성시킵니다.</Text>
                 </IntroText>
                 <Img active={active}></Img>
-                {active && <Footer palette={palette}>
-                        <Box1 palette={palette}></Box1>
-                        <User palette={palette}><Title2 palette={palette}>위뮤즈 가입자 수</Title2><Text2 palette={palette}>500명</Text2></User>
-                        <With palette={palette}><Title2 palette={palette}>함께한 공연 수</Title2><Text2 palette={palette}>500회</Text2></With>
-                        <Like palette={palette}><Title2 palette={palette}>평균 공연 만족도</Title2><Text2 palette={palette}>100%</Text2></Like>
-                        <Box2 palette={palette}></Box2>
+                {active && <Footer>
+                        <Box1></Box1>
+                        <User><Title2>위뮤즈 가입자 수</Title2><Text2>500명</Text2></User>
+                        <With><Title2>함께한 공연 수</Title2><Text2>500회</Text2></With>
+                        <Like><Title2 >평균 공연 만족도</Title2><Text2 >100%</Text2></Like>
+                        <Box2></Box2>
                     </Footer>
                 }
             </div>
@@ -39,13 +39,13 @@ const Block = styled.section<{ active: boolean }>`
     height: 100%;
     background-size: contain;
     background: ${({ active }) =>
-        active ? `url(${IntroImg2})` : palette.white0};
+        active ? `url(${introimg2})` : palette.white0};
     transition: background-color 0.2s;
     background-size: cover;
     background-position: center;
     ${media.mobile} {
         background-image: ${({active}) =>
-        active ? `url(${IntroMob2})` : ''};
+        active ? `url(${intromobimg2})` : ''};
 `;
 
 const IntroText = styled.div<{active:boolean}>`
@@ -87,14 +87,14 @@ const Img = styled.div<{active:boolean}>`
     height: 450px;
     background-size:cover;
     background-image: ${({active}) =>
-        active ? '' : `url(${IntroImg})`};
+        active ? '' : `url(${introimg1})`};
     ${media.mobile} {
         background-image: ${({active}) =>
-            active ? '' : `url(${IntroMob})`};
+            active ? '' : `url(${intromobimg1})`};
     }
 `
 
-const Footer = styled.div<{palette:any}>`
+const Footer = styled.div`
     display: flex;
     width: 100%;
     position: absolute;
@@ -106,7 +106,7 @@ const Footer = styled.div<{palette:any}>`
     }
 `
 
-const Box1 = styled.div<{palette:any}>`
+const Box1 = styled.div`
     background-color:${palette.black1};
     opacity: 0.8;
     backdrop-filter:blur(15px);
@@ -117,7 +117,7 @@ const Box1 = styled.div<{palette:any}>`
         display:none;
     }
 `
-const User = styled.div<{palette:any}>`
+const User = styled.div`
     background-color:${palette.black1};
     display: flex;
     flex-direction: column;
@@ -132,7 +132,7 @@ const User = styled.div<{palette:any}>`
         width:100%;
     }
 `
-const With = styled.div<{palette:any}>`
+const With = styled.div`
     background-color:${palette.black1};
     display: flex;
     flex-direction: column;
@@ -147,7 +147,7 @@ const With = styled.div<{palette:any}>`
         width:100%;
     }
 `
-const Like = styled.div<{palette:any}>`
+const Like = styled.div`
     background-color:${palette.black1};
     display: flex;
     flex-direction: column;
@@ -157,12 +157,12 @@ const Like = styled.div<{palette:any}>`
     backdrop-filter:blur(15px);
     width: 360px;
     height:130px;
-    border: 0.25px solid ${palette.gray2};
+    border: 1px solid ${palette.gray2};
     ${media.mobile} {
         width:100%;
     }
 `
-const Box2 = styled.div<{palette:any}>`
+const Box2 = styled.div`
     background-color:${palette.black1};
     opacity: 0.8;
     backdrop-filter:blur(15px);
@@ -174,13 +174,13 @@ const Box2 = styled.div<{palette:any}>`
     }
 `
 
-const Title2 = styled.div<{palette:any}>`
+const Title2 = styled.div`
     font-size: 12px;
     font-weight: lighter;
     padding-bottom: 15px;
     color:${palette.white0};
 `
-const Text2 = styled.div<{palette:any}>`
+const Text2 = styled.div`
     font-size: 23px;
     font-weight: bold;
     color:${palette.white0};

1ee255d9ffdd1a5127b648ba8cfccb36383b8513
seungiminRyu <fbtmdals011@naver.com>
2022-12-06 11:53:24 +0900

    Create ChocomusicArticles.tsx

---
+++ src/components/chocomusic/ChocomusicArticles.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export type ChocomusicArticlesProps = {};
+
+function ChocomusicArticles(props: ChocomusicArticlesProps) {
+    return (
+        <Block>
+            <></>
+        </Block>
+    );
+}
+
+const Block = styled.div``;
+
+export default ChocomusicArticles;

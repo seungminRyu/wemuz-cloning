7bb112f7c7098753604bdaf3eb9bf781029c5f1c
seungiminRyu <fbtmdals011@naver.com>
2022-12-06 11:53:30 +0900

    Create ChocomusicFooter.tsx

---
+++ src/components/chocomusic/ChocomusicFooter.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export type ChocomusicFooterProps = {};
+
+function ChocomusicFooter(props: ChocomusicFooterProps) {
+    return (
+        <Block>
+            <></>
+        </Block>
+    );
+}
+
+const Block = styled.div``;
+
+export default ChocomusicFooter;
